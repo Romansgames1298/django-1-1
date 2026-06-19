993d8e5b040e9446a15bf0763ad5b1a6fe62a52f
Romansgames1298 <haltsevroman04@gmail.com>
2026-06-19 14:04:04 +0300

    New project

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.role})"
